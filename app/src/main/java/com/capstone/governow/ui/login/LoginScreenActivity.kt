93ae54719116d02d5e9a18befbf05a356ed900e0
iqbaal230902 <iqbaal230902@gmail.com>
2024-06-03 00:08:13 +0700

    first commit

---
+++ app/src/main/java/com/capstone/governow/ui/login/LoginScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.governow.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.governow.R
+
+class LoginScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
---
+++ app/src/main/java/com/capstone/governow/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.governow.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.governow.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
